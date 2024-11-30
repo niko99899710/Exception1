5fbe28d8f987f2acab81689af0bf246250717413
karinaid <bagdasarova-k@inbox.ru>
2024-11-30 22:03:38 +0400

    H/W 2/2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int ring = 16;
+        if (ring % 2 == 0) {
+            System.out.println("Этот размер есть в наличие");
+        } else {
+            throw new ArithmeticException("Такого размера нет в даной линейке") {
+            };
+        }
+        System.out.println(isNumber("88"));
     }
+
+    public static String isNumber(String sentence1) {
+        try {
+            Integer.parseInt(sentence1);
+            return "Цифры есть";
+        } catch (NumberFormatException e) {
+            return "Цифр нет";
+        }
+   }
 }
+
+
+
+
